17a6b0be62358462e0c700c5a7617b87ea8c7cd6
Elisha Senoo <elisha@rightcomtech.com>
2013-11-20 23:48:19 +0000

    add users migration added

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost:3308',
+			'host'      => 'localhost',
 			'database'  => 'mystudychumdb',
 			'username'  => 'root',
 			'password'  => 'james417',
