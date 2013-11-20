2eb2e1181b27935de2123375064679aa6a900142
Elisha Senoo <elisha@rightcomtech.com>
2013-11-20 22:23:20 +0000

    signin and signup views added

--- app/config/database.php
+++ app/config/database.php
@@ -68,7 +68,7 @@ return array(
 			'host'     => 'localhost',
 			'database' => 'database',
 			'username' => 'root',
-			'password' => '',
+			'password' => 'james',
 			'charset'  => 'utf8',
 			'prefix'   => '',
 			'schema'   => 'public',
