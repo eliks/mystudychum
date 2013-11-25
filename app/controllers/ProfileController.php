2a68a8e5b0327007e9c06db7e8f7933e2c737c6c
Elisha Senoo <elisha@rightcomtech.com>
2013-11-25 13:30:46 +0000

    profile submission

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -3,17 +3,6 @@
 class ProfileController extends BaseController {
 	public $restful = true;
 	
-	public static $rules = array(
-	   'first_name'=>'required|alpha|min:2',
-	   'last_name'=>'required|alpha|min:2',
-	   'email'=>'required|email',
-	   'DOB'=>'required|date',
-	   'country_id' => 'required',
-	   'education' => 'required|in:Other,High School,High School Graduate,College,College Graduate,',
-	   'gender' => 'required|in:Male,Female',
-	   'tags' => 'required'
-	   );
-	
 	public function get_index(){
 		$temp_view =  View::make('Profile.index');
 		$temp_view->user_email = Session::get('email');
@@ -27,7 +16,19 @@ class ProfileController extends BaseController {
 	} 
 	
 	public function submit(){
-		$user = new User();
+		
+		$user = DB::table('users')->where('email', Session::get('email'))->first();
+		
+		$rules = array(
+		   'first_name'=>'required|alpha|min:2',
+		   'last_name'=>'required|alpha|min:2',
+		   'email'=>'required|email|unique:users,email,$user->id',
+		   'DOB'=>'required|date',
+		   'country_id' => 'required',
+		   'education' => 'required|in:Other,High School,High School Graduate,College,College Graduate,',
+		   'gender' => 'required|in:Male,Female',
+		   'tags' => 'required'
+		   );
 		
 		$user->first_name = Input::get('first_name');
 		$user->last_name = Input::get('last_name');
@@ -37,7 +38,7 @@ class ProfileController extends BaseController {
 		$user->education = Input::get('education');
 		$user->gender = Input::get('gender');
 
-		$validator = Validator::make(Input::all(), $this::$rules);
+		$validator = Validator::make(Input::all(), $rules);
 		
 		if ($validator->passes()) {
 			// validation has passed, save user in DB
