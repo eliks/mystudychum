0bfed28b7f7876193decb5eebfd6f78a6f02a543
EC2 Default User <ec2-user@ip-172-31-15-238.us-west-2.compute.internal>
2013-11-27 12:04:12 +0000

    chum interests table migration added

---
+++ app/database/migrations/2013_11_27_120320_create_chum_interests_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChumInterestsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
