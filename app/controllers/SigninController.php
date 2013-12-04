218078d114f010e51f0eead9dca1bc8fcce3923c
Elisha Senoo <elisha@rightcomtech.com>
2013-12-04 09:07:19 +0000

    debugging for demo

--- app/controllers/SigninController.php
+++ app/controllers/SigninController.php
@@ -37,7 +37,7 @@ class SigninController extends BaseController {
 					$users = User::all();
 					$user = User::where("email",Session::get("email"))->get()->first();
 					$data = array("user"=>$user,"users"=>$users);
-					return Redirect::to('Chums.index',$data);
+					return View::make('Chums.index',$data);
 					// return View::make('Profile.edit',$data);
 				   
 				} else {
