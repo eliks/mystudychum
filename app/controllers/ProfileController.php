bdb0b8df185afa11dcc7a0dff15c853e5d50e1d3
Elisha Senoo <elisha@rightcomtech.com>
2013-12-04 10:59:31 +0000

    debugging for demo

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -30,7 +30,7 @@ class ProfileController extends BaseController {
 		$user = User::where("email",Session::get("email"))->get()->first();
 		
 		$rules = array(
-		   'profile_image'=>'required|image',
+		   'profile_image'=>'image',
 		   'first_name'=>'required|alpha|min:2',
 		   'last_name'=>'required|alpha|min:2',
 		   'email'=>'required|email|unique:users,email,'.$user['id'],
