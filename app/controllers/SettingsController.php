c9ad59cdb9363c1723095e5ab097d06d682d7d19
Elisha Senoo <elisha@rightcomtech.com>
2013-12-04 10:21:26 +0000

    debugging for demo

--- app/controllers/SettingsController.php
+++ app/controllers/SettingsController.php
@@ -7,8 +7,9 @@ class SettingsController extends BaseController {
 		// $temp_view =  View::make('Settings.index');
 		// $temp_view->user = DB::table('users')->where('email', Session::get('email'))->first();
 		// return $temp_view;
-		Session::put('email', $user->email);
 		$user = User::where("email",Session::get("email"))->get()->first();
+		Session::put('email', $user->email);
+		
 		$data = array("user"=>$user);
 
 		return View::make('Settings.index',$data);
